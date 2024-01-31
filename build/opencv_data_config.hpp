d6e86c817b98e5c10d86db6ae31413cc842337c3
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2024-01-31 13:51:09 +0600

    Add files via upload

---
+++ build/opencv_data_config.hpp
@@ -0,0 +1,11 @@
+
+#define OPENCV_INSTALL_PREFIX "C:/opencvCUDA/build/install"
+
+#define OPENCV_DATA_INSTALL_PATH "etc"
+
+#define OPENCV_BUILD_DIR "C:/opencvCUDA/build"
+
+#define OPENCV_DATA_BUILD_DIR_SEARCH_PATHS \
+    "../opencv-4.9.0/opencv-4.9.0/"
+
+#define OPENCV_INSTALL_DATA_DIR_RELATIVE "../../../etc"
---
+++ build/opencv_tests_config.hpp
@@ -0,0 +1,4 @@
+
+#define OPENCV_INSTALL_PREFIX "C:/opencvCUDA/build/install"
+
+#define OPENCV_TEST_DATA_INSTALL_PATH "testdata"
